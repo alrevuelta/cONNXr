6de8e86fd432781cb75ce8db4bec6c1f00ace737
Alvaro <alvrevuelta@gmail.com>
2020-02-19 15:24:54 +0100

    Update tests.c

--- test/tests.c
+++ test/tests.c
@@ -660,7 +660,7 @@ int main(int argc, char **argv)
 
   CU_cleanup_registry();
   
-  printf("CU_get_number_of_tests_failed is %d\n", CU_get_number_of_tests_failed);
+  printf("CU_get_number_of_tests_failed is %d\n", CU_get_number_of_tests_failed());
   // TODO Temporal hackish solution to force CI to fail
   if (CU_get_number_of_tests_failed != 0){
     exit (1);
