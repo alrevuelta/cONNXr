4c7e36c5b48fbe82ee311f050a23c68659b42bc9
nopeslide <drechsler@tu-berlin.de>
2020-04-20 18:03:06 +0200

    refactored cli and added README

---
+++ scripts/onnx_generator/README.md
@@ -0,0 +1,69 @@
+# Onnx File Generator
+
+- uses onnx schema files
+- generates operator header files
+- generates operator stubs
+- generates operator sanity checks
+- generates operator type resolver
+- generates operator name resolver
+
+
+## Usage CLI
+
+```
+$ python -m onnx_generator -h
+usage: onnx_generator [-h] [--onnx <path>] [-v] [--header <path>] [--src <path>] [-i <regex> [<regex> ...]]
+                      [-e <regex> [<regex> ...]]
+                      [--] <path>
+
+positional arguments:
+  [--] <path>           where to put generated files
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --onnx <path>         use custom onnx_cpp2py_export.so located in <path> dir
+  -v, --verbose         verbose output
+  --header <path>       where to put headers in main path (default: include/operators)
+  --src <path>          where to put sources in main path (default: src/operators)
+  -i <regex> [<regex> ...], --include <regex> [<regex> ...]
+                        operators to include (default: ['.*'])
+  -e <regex> [<regex> ...], --exclude <regex> [<regex> ...]
+                        already included operators to exclude (default: [])
+```
+
+## Usage python
+
+```python
+import onnx_generator
+import onnx_generator.args as args
+print(args.path)
+### []
+print(args.onnx)
+### []
+print(args.include)
+### ['.*']
+print(args.exclude)
+### []
+print(args.src)
+### ['src/operators']
+print(args.header)
+### ['include/operators']
+print(args.verbose)
+### 0
+import onnx_generator.run as run
+### including onnx operator schemas
+### pattern '.*' included 324 operator schemas
+### result: 324 onnx operator schemas
+### excluding onnx operator schemas
+### result: 324 onnx operator schemas
+### generating onnx operator headers
+### skipping write because args.path not set
+### generating onnx operator implementations
+### skipping write because args.path not set
+len(run.schemas)
+### 324
+len(run.headers)
+### 324
+len(run.implementations)
+### 324
+```
--- scripts/onnx_generator/__main__.py
+++ scripts/onnx_generator/__main__.py
@@ -1,108 +1,56 @@
 import argparse
-import os
-import sys
-import re
-from .OperatorHeader import OperatorHeader
-from .OperatorImplementation import OperatorImplementation
-from .OnnxWrapper import OnnxSchema
+from . import args
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser("onnx_generator")
 parser.add_argument(
     "--onnx",
     nargs=1,
     metavar="<path>",
+    default = args.onnx,
     help="use custom onnx_cpp2py_export.so located in <path> dir"
 )
 parser.add_argument(
     "-v", "--verbose",
-    action='count', default=0,
+    action='count',
+    default=args.verbose,
     help="verbose output"
 )
 parser.add_argument(
     "--header",
     nargs=1,
     metavar="<path>",
-    default=["include/operators"],
-    help="where to put headers in main path"
+    default=args.header,
+    help=f"where to put headers in main path (default: {args.header[0]})"
 )
 parser.add_argument(
     "--src",
     nargs=1,
     metavar="<path>",
-    default=["src/operators"],
-    help="where to put sources in main path"
+    default=args.src,
+    help=f"where to put sources in main path (default: {args.src[0]})"
 )
 parser.add_argument(
     "-i", "--include",
     metavar="<regex>",
     nargs='+',
-    default=[".*"],
-    help="operators to include (default: '.*')"
+    default=args.include,
+    help=f"operators to include (default: {args.include})"
 )
 parser.add_argument(
     "-e", "--exclude",
     nargs='+',
     metavar="<regex>",
-    help="already included operators to exclude (default: None)"
+    default=args.exclude,
+    help=f"already included operators to exclude (default: {args.exclude})"
 )
 parser.add_argument(
     'path',
     metavar="[--] <path>",
+    default=args.path,
     nargs=1,
     help="where to put generated files"
 )
-args = parser.parse_args()
 
-print(args)
+args.__dict__.update(parser.parse_args().__dict__)
 
-if args.onnx:
-    sys.path.insert(0, os.path.realpath(args.onnx[0]))
-    import onnx_cpp2py_export
-else:
-    from onnx import onnx_cpp2py_export
-
-all_schemas = [ OnnxSchema(s) for s in onnx_cpp2py_export.defs.get_all_schemas_with_history()]
-
-schemas = []
-for pattern in args.include:
-  included = list(filter(lambda s: re.match(pattern,s.operator_name) != None,all_schemas))
-  for s in included:
-    if (args.verbose):
-      print(f"pattern '{pattern}' included '{s.operator_name}'")
-    if s not in schemas:
-      schemas.append(s)
-  print(f"pattern '{pattern}' included {len(included)} operators")
-
-
-if args.exclude:
-  for pattern in args.exclude:
-    excluded = list(filter(lambda s: re.match(pattern,s.operator_name) != None ,schemas))
-    for s in excluded:
-      if args.verbose:
-        print(f"pattern '{pattern}' excluded '{s.operator_name}'")
-      schemas.remove(s)
-  print(f"pattern '{pattern}' excluded {len(excluded)} operators")
-
-headers = [ OperatorHeader(s,args.header[0]) for s in schemas ]
-
-for h in headers:
-  filename = os.path.normpath(f"{h.schema.operator_name}.h")
-  dirname = os.path.realpath(f"{args.path[0]}/{args.header[0]}/{h.schema.domain}/")
-  filepath = os.path.realpath(f"{dirname}/{filename}")
-  if args.verbose:
-    print(f"writing header {filepath}")
-  os.makedirs(dirname,exist_ok=True)
-  open(filepath,"w").write(h.text())
-print(f"wrote {len(headers)} headers")
-
-implementations = [ OperatorImplementation(s,args.header[0]) for s in schemas ]
-
-for imp in implementations:
-  filename = os.path.normpath(f"{imp.schema.operator_name}.c")
-  dirname = os.path.realpath(f"{args.path[0]}/{args.src[0]}/{imp.schema.domain}/")
-  filepath = os.path.realpath(f"{dirname}/{filename}")
-  if args.verbose:
-    print(f"writing source {filepath}")
-  os.makedirs(dirname,exist_ok=True)
-  open(filepath,"w").write(imp.text())
-print(f"wrote {len(headers)} source")
+import run
