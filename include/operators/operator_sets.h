f87a88c4a4597ef8f32b51b81dd02afeeb58d3d3
nopeslide <drechsler@tu-berlin.de>
2020-06-26 13:55:47 +0200

    made operator_info optional

--- include/operators/operator_sets.h
+++ include/operators/operator_sets.h
@@ -1,17 +1,25 @@
 #ifndef OPERATOR_SETS_H
 #define OPERATOR_SETS_H
 
-#include "operators/operator_info.h"
-
 typedef struct operator_set       operator_set;
 typedef struct operator_sets      operator_sets;
+typedef struct operator_set_entry operator_set_entry;
+
+#include "operators/operator_info.h"
+#include "operators/operator.h"
+
+struct operator_set_entry {
+    char             *name;
+    operator_resolver resolver;
+    operator_info    *info;
+};
 
 struct operator_set
 {
-    size_t         version;
-    char          *domain;
-    size_t         length;
-    operator_info *entries[];
+    size_t              version;
+    char               *domain;
+    size_t              length;
+    operator_set_entry *entries[];
 };
 
 struct operator_sets
