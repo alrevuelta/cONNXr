fe45b32b9c27ca19767acc4eef0d72d2c2cb2928
nopeslide <drechsler@tu-berlin.de>
2020-07-08 13:13:00 +0200

    added 'clip' and 'globalaveragepool'

--- src/operators/operator_sets.c
+++ src/operators/operator_sets.c
@@ -4,12 +4,14 @@
 #include "operators/onnx/operator__onnx__matmul__9.h"
 #include "operators/onnx/operator__onnx__maxpool__12.h"
 #include "operators/onnx/operator__onnx__transpose__1.h"
+#include "operators/onnx/operator__onnx__clip__12.h"
 #include "operators/onnx/operator__onnx__relu__6.h"
 #include "operators/onnx/operator__onnx__conv__11.h"
 #include "operators/onnx/operator__onnx__argmax__12.h"
 #include "operators/onnx/operator__onnx__add__7.h"
 #include "operators/onnx/operator__onnx__leakyrelu__6.h"
 #include "operators/onnx/operator__onnx__reshape__5.h"
+#include "operators/onnx/operator__onnx__globalaveragepool__1.h"
 #include "operators/onnx/operator__onnx__mul__7.h"
 #include "operators/onnx/operator__onnx__constant__12.h"
 #include "operators/onnx/operator__onnx__batchnormalization__9.h"
@@ -19,12 +21,16 @@
 operator_set operator_set__onnx__1 = {
   .version = 1,
   .domain  = "onnx",
-  .length  = 1,
+  .length  = 2,
   .entries = {
     {
   .name = "Transpose",
   .resolver = (operator_resolver) resolve_operator__onnx__transpose__1,
   .info = &info_operator__onnx__transpose__1
+},{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
 }
   }
 };
@@ -32,7 +38,7 @@ operator_set operator_set__onnx__1 = {
 operator_set operator_set__onnx__5 = {
   .version = 5,
   .domain  = "onnx",
-  .length  = 2,
+  .length  = 3,
   .entries = {
     {
   .name = "Transpose",
@@ -42,6 +48,10 @@ operator_set operator_set__onnx__5 = {
   .name = "Reshape",
   .resolver = (operator_resolver) resolve_operator__onnx__reshape__5,
   .info = &info_operator__onnx__reshape__5
+},{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
 }
   }
 };
@@ -49,7 +59,7 @@ operator_set operator_set__onnx__5 = {
 operator_set operator_set__onnx__6 = {
   .version = 6,
   .domain  = "onnx",
-  .length  = 5,
+  .length  = 6,
   .entries = {
     {
   .name = "Transpose",
@@ -68,6 +78,10 @@ operator_set operator_set__onnx__6 = {
   .resolver = (operator_resolver) resolve_operator__onnx__reshape__5,
   .info = &info_operator__onnx__reshape__5
 },{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
+},{
   .name = "Sigmoid",
   .resolver = (operator_resolver) resolve_operator__onnx__sigmoid__6,
   .info = &info_operator__onnx__sigmoid__6
@@ -78,7 +92,7 @@ operator_set operator_set__onnx__6 = {
 operator_set operator_set__onnx__7 = {
   .version = 7,
   .domain  = "onnx",
-  .length  = 7,
+  .length  = 8,
   .entries = {
     {
   .name = "Transpose",
@@ -101,6 +115,10 @@ operator_set operator_set__onnx__7 = {
   .resolver = (operator_resolver) resolve_operator__onnx__reshape__5,
   .info = &info_operator__onnx__reshape__5
 },{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
+},{
   .name = "Mul",
   .resolver = (operator_resolver) resolve_operator__onnx__mul__7,
   .info = &info_operator__onnx__mul__7
@@ -115,7 +133,7 @@ operator_set operator_set__onnx__7 = {
 operator_set operator_set__onnx__9 = {
   .version = 9,
   .domain  = "onnx",
-  .length  = 9,
+  .length  = 10,
   .entries = {
     {
   .name = "MatMul",
@@ -142,6 +160,10 @@ operator_set operator_set__onnx__9 = {
   .resolver = (operator_resolver) resolve_operator__onnx__reshape__5,
   .info = &info_operator__onnx__reshape__5
 },{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
+},{
   .name = "Mul",
   .resolver = (operator_resolver) resolve_operator__onnx__mul__7,
   .info = &info_operator__onnx__mul__7
@@ -160,7 +182,7 @@ operator_set operator_set__onnx__9 = {
 operator_set operator_set__onnx__11 = {
   .version = 11,
   .domain  = "onnx",
-  .length  = 11,
+  .length  = 12,
   .entries = {
     {
   .name = "MatMul",
@@ -191,6 +213,10 @@ operator_set operator_set__onnx__11 = {
   .resolver = (operator_resolver) resolve_operator__onnx__reshape__5,
   .info = &info_operator__onnx__reshape__5
 },{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
+},{
   .name = "Mul",
   .resolver = (operator_resolver) resolve_operator__onnx__mul__7,
   .info = &info_operator__onnx__mul__7
@@ -213,7 +239,7 @@ operator_set operator_set__onnx__11 = {
 operator_set operator_set__onnx__12 = {
   .version = 12,
   .domain  = "onnx",
-  .length  = 14,
+  .length  = 16,
   .entries = {
     {
   .name = "MatMul",
@@ -227,6 +253,10 @@ operator_set operator_set__onnx__12 = {
   .name = "Transpose",
   .resolver = (operator_resolver) resolve_operator__onnx__transpose__1,
   .info = &info_operator__onnx__transpose__1
+},{
+  .name = "Clip",
+  .resolver = (operator_resolver) resolve_operator__onnx__clip__12,
+  .info = &info_operator__onnx__clip__12
 },{
   .name = "Relu",
   .resolver = (operator_resolver) resolve_operator__onnx__relu__6,
@@ -251,6 +281,10 @@ operator_set operator_set__onnx__12 = {
   .name = "Reshape",
   .resolver = (operator_resolver) resolve_operator__onnx__reshape__5,
   .info = &info_operator__onnx__reshape__5
+},{
+  .name = "GlobalAveragePool",
+  .resolver = (operator_resolver) resolve_operator__onnx__globalaveragepool__1,
+  .info = &info_operator__onnx__globalaveragepool__1
 },{
   .name = "Mul",
   .resolver = (operator_resolver) resolve_operator__onnx__mul__7,
