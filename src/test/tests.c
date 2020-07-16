458eaa387629107b5e73c955663df5672e8988af
alrevuelta <alvrevuelta@gmail.com>
2020-07-16 18:42:49 +0200

    Comment mobilenetv2
    Comment mobilenetv2

--- src/test/tests.c
+++ src/test/tests.c
@@ -639,7 +639,7 @@ int main(int argc, char **argv)
   CU_add_test(modelsTestSuite, "test_model_mnist", test_model_mnist);
   CU_add_test(modelsTestSuite, "test_model_tinyyolov2", test_model_tinyyolov2);
   CU_add_test(modelsTestSuite, "test_model_super_resolution", test_model_super_resolution);
-  CU_add_test(modelsTestSuite, "test_model_mobilenetv2", test_model_mobilenetv2);
+  //CU_add_test(modelsTestSuite, "test_model_mobilenetv2", test_model_mobilenetv2);
 
   CU_basic_set_mode(CU_BRM_VERBOSE);
 
