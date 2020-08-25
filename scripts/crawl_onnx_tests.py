3f1b0f2b47f19e6fe1fca87a22f2357005d6ce0e
alrevuelta <alvrevuelta@gmail.com>
2020-08-25 22:50:53 +0200

    Add args parser
    Add some custom input parameters

--- scripts/crawl_onnx_tests.py
+++ scripts/crawl_onnx_tests.py
@@ -7,58 +7,54 @@ import glob
 import onnx
 
 def parse_args():  # type: () -> argparse.Namespace
-    """
-    parser = argparse.ArgumentParser('backend-test-tools')
-    subparsers = parser.add_subparsers()
-
-    subparser = subparsers.add_parser('generate-data', help='convert testcases to test data')
-    subparser.add_argument('-o', '--output', default=DATA_DIR,
-                           help='output directory (default: %(default)s)')
-    subparser.set_defaults(func=generate_data)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--release', help='Set for a specific release. By default all releases are crawled. Must match a release existing on PyPI')
+    parser.add_argument('--domain', help='Set to fetch only a specific domain ai.onnx|ai.onnx.ml|ai.onnx.preview.training')
+    parser.add_argument('--folder', default='test_data/onnx_backend', help='Output folder to place the tests')
     return parser.parse_args()
-    """
 
 
-# Hardcoded. Replace once #2918 is merged
-# This tabled can be accessed programatically
+# This table can be accessed programatically (see onnx #2918)
+# However I suspect there is a bug with the '1.2' entry, since such version
+# does not exist
 VERSION_TABLE = [
     # Release-version, IR version, ai.onnx version, ai.onnx.ml version, (optional) ai.onnx.training version
-    #('1.0', 3, 1, 1),
-    #('1.1', 3, 5, 1),
-    #('1.1.2', 3, 6, 1),
-    #('1.2', 3, 7, 1), # TODO op 7 is missing. Is the table right? There is relase that matches 1.2
+    ('1.0', 3, 1, 1),
+    ('1.1', 3, 5, 1),
+    ('1.1.2', 3, 6, 1),
+    ('1.2.3', 3, 7, 1), # The original table is 1.2 but that release does not exist
     ('1.3', 3, 8, 1),
     ('1.4.1', 4, 9, 1),
     ('1.5.0', 5, 10, 1),
     ('1.6.0', 6, 11, 2),
-    #('1.7.0', 7, 12, 2, 1)
+    ('1.7.0', 7, 12, 2, 1)
 ]
 
-TEMP_DIR = "older_versions"
-#OUTPUT_DIR = "test_data/onnx_backend"
-OUTPUT_DIR = "remove"
+TEMP_DIR = "temp"
 PYTHON_TESTS = "onnx/backend/test/data/node/"
-
-map_domain_name = {"": "ai.onnx",
-                  "xx": "ai.onnx.ml",
-                  "xxx": "ai.onnx.training"}
 
 def download(onnx_version):
     os.system(f"pip download --no-binary=:all: --no-deps -d {TEMP_DIR}/{onnx_version} onnx=={onnx_version}")
 
 def main():  # type: () -> None
-    #args = parse_args()
-    #args.func(args)
-    for version in VERSION_TABLE:
+    args = parse_args()
+    if not args.release:
+        releases = [i[0] for i in VERSION_TABLE]
+    else:
+        releases = [args.release]
+        print(releases)
+
+    # Iterate the PyPI releases
+    for release in releases:
         # Download the Python release
-        download(version[0])
+        download(release)
 
-        # Untar the files in a temp dir
-        tar_file_path = glob.glob(f'{TEMP_DIR}/{version[0]}/onnx-{version[0]}*.tar.gz')[0]
+        # Untar the files
+        tar_file_path = glob.glob(f'{TEMP_DIR}/{release}/onnx-{release}*.tar.gz')[0]
         folder_name = tar_file_path.replace(".tar.gz", "")
-        os.system(f"tar -zxvf {tar_file_path} -C {TEMP_DIR}/{version[0]}")
+        os.system(f"tar -zxvf {tar_file_path} -C {TEMP_DIR}/{release}")
 
+        # Path to the tests folder containing all the onnx and pb files
         tests_path = os.path.join(folder_name, PYTHON_TESTS)        
 
         # Iterate test cases of a given release
@@ -68,7 +64,7 @@ def main():  # type: () -> None
 
                 # Check if the test contains a model file. Note that onnx is used
                 # in the last releases but .pb is used in the first ones
-                included_extensions = ['model.onnx','node.pb']
+                included_extensions = ['model.onnx', 'node.pb']
                 file_names = [fn for fn in os.listdir(test_folder_full) if any(fn.endswith(ext) for ext in included_extensions)]
 
                 if len(file_names) != 1:
@@ -79,39 +75,44 @@ def main():  # type: () -> None
                 onnx_model = onnx.load(onnx_path)
 
                 if len(onnx_model.opset_import) != 1:
-                    print(dir(onnx_model))
-                    print(onnx_model.model_version)
-                    print(onnx_model.opset_import)
+                    # TODO v1.0 and v1.1 don't use onnx but pb. Seems that this field
+                    # is empty
                     raise Exception("Opset imported different than one")
 
                 # Check which opset is imported
                 opset_version = onnx_model.opset_import[0].version
+                domain_name = onnx_model.opset_import[0].domain
 
-                # Ouput folder
-                output_path = os.path.join(OUTPUT_DIR,
-                                           map_domain_name[onnx_model.domain],
-                                           str(opset_version),
-                                           test_folder)                
+                # Base domain ai.onnx is empty
+                if domain_name == "":
+                    domain_name = "ai.onnx"
+
+                # Just skip if a specific domain is provided
+                if args.domain and args.domain != domain_name:
+                    continue
+
+                # Copy the files
+                # Output folder
+                output_path = os.path.join(args.folder,
+                                        domain_name,
+                                        str(opset_version),
+                                        test_folder)                
 
                 # Folder for domain
-                domain_path = os.path.join(OUTPUT_DIR,
-                                           map_domain_name[onnx_model.domain])
+                domain_path = os.path.join(args.folder,
+                                        domain_name)
 
                 # Folder for opset 
-                opset_path = os.path.join(OUTPUT_DIR,
-                                          map_domain_name[onnx_model.domain],
-                                          str(opset_version))
+                opset_path = os.path.join(args.folder,
+                                        domain_name,
+                                        str(opset_version))
 
-                print("creating folder domain", domain_path)
-                print("creaging folder, opset", opset_path)
-                print("copy", test_folder_full, "to", output_path)
                 os.system(f"mkdir {domain_path}")
                 os.system(f"mkdir {opset_path}")
                 os.system(f"cp -R {test_folder_full} {output_path}")
 
     #Commented for testing
     #os.system(f"rm -r {TEMP_DIR}")
-
 
 if __name__ == '__main__':
     main()
