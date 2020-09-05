3c6a761ace43b255e8dd0b5cf7061af96f61bf52
alrevuelta <alvrevuelta@gmail.com>
2020-09-05 18:01:15 +0200

    Minor refactoring
    Minor refactoring

--- test_data/get_official_tests.py
+++ test_data/get_official_tests.py
@@ -15,15 +15,20 @@ contains the model and a set of inputs and expected outputs. Once the data is fe
 organizes in folders the test cases according to its domain and opset version.
 """
 
+
 def parse_args():  # type: () -> argparse.Namespace
     parser = argparse.ArgumentParser()
-    parser.add_argument('--release', help='Set for a specific release. By default all releases are crawled. Must match a release existing on PyPI')
-    parser.add_argument('--domain', help='Set to fetch only a specific domain ai.onnx|ai.onnx.ml|ai.onnx.preview.training')
+    parser.add_argument('--release',
+                        help='Set for a specific release. By default all releases are crawled. Must match a release existing on PyPI')
+    parser.add_argument('--domain',
+                        help='Set to fetch only a specific domain ai.onnx|ai.onnx.ml|ai.onnx.preview.training')
     parser.add_argument('--modelpath', default='test_data/node_official_data', help='Output folder to place the models')
     parser.add_argument('--casepath', default='test_data/node_official_case', help='Output folder to place the scripts')
-    parser.add_argument('--getcase', default=False, const=True, nargs='?', help='Downloads also the Python scripts that generates the model')
-    # TODO Add --getdata
+    parser.add_argument('--getdata', default=False, const=True, nargs='?', help='Downloads the pb test files')
+    parser.add_argument('--getcase', default=False, const=True, nargs='?',
+                        help='Downloads the Python scripts that generate the models')
     return parser.parse_args()
+
 
 """
 This table can be accessed programatically (see onnx #2918)
@@ -34,10 +39,10 @@ This table must be manually updated if a new onnx version is released.
 """
 VERSION_TABLE = [
     # Release-version, IR version, ai.onnx version, ai.onnx.ml version, (optional) ai.onnx.training version
-    #('1.0', 3, 1, 1),
-    #('1.1', 3, 5, 1),
+    # ('1.0', 3, 1, 1),
+    # ('1.1', 3, 5, 1),
     ('1.1.2', 3, 6, 1),
-    ('1.2.3', 3, 7, 1), # The original table is 1.2 but that release does not exist
+    ('1.2.3', 3, 7, 1),  # The original table is 1.2 but that release does not exist
     ('1.3', 3, 8, 1),
     ('1.4.1', 4, 9, 1),
     ('1.5.0', 5, 10, 1),
@@ -55,8 +60,10 @@ PYTHON_CASE = "onnx/backend/test/case/node/"
 
 ALL_SCHEMAS = onnx_cpp2py_export.defs.get_all_schemas_with_history()
 
+
 def download(onnx_version):
     os.system(f"pip download --no-binary=:all: --no-deps -d {TEMP_DIR} onnx=={onnx_version}")
+
 
 def copy_files(domain, opset, test_name, test_folder, out_folder):
     # Copy model files by default
@@ -75,17 +82,96 @@ def copy_files(domain, opset, test_name, test_folder, out_folder):
 
     os.makedirs(domain_path, exist_ok=True)
     os.makedirs(opset_path, exist_ok=True)
-                
+
     try:
         shutil.copytree(test_folder, output_path)
-        print("copying", test_folder, output_path)
     except FileExistsError:
-        print("failed", test_folder, output_path)
         print("Ignoring", output_path, "already exists")
+
+
+def get_data(tests_path, domain, out_path):
+    # Iterate test cases of a given release
+    for test_folder in os.listdir(tests_path):
+        test_folder_full = os.path.join(tests_path, test_folder)
+        if os.path.isdir(test_folder_full):
+
+            # Check if the test contains a model file. Note that onnx is used
+            # in the last releases but .pb is used in the first ones
+            included_extensions = ['model.onnx', 'node.pb']
+            file_names = [fn for fn in os.listdir(test_folder_full) if
+                          any(fn.endswith(ext) for ext in included_extensions)]
+
+            if len(file_names) != 1:
+                raise Exception("Model file not found")
+
+            #  Open the onnx file
+            onnx_path = os.path.join(tests_path, test_folder, file_names[0])
+            onnx_model = onnx.load(onnx_path)
+
+            if len(onnx_model.opset_import) != 1:
+                raise Exception("Opset imported different than one")
+
+            # Check which opset is imported
+            opset_version = onnx_model.opset_import[0].version
+            domain_name = onnx_model.opset_import[0].domain
+
+            # Base domain ai.onnx is empty
+            if domain_name == "":
+                domain_name = "ai.onnx"
+
+            # Just skip if a specific domain is provided
+            if domain and domain != domain_name:
+                continue
+
+            copy_files(domain_name, opset_version, test_folder,
+                       test_folder_full, out_path)
+
+def get_node(node_path, domain, out_path, version):
+    for test in os.listdir(node_path):
+        if test != "__init__.py":
+            case_full_path = os.path.join(node_path, test)
+
+            case_f = open(case_full_path, 'r')
+            content = case_f.read().replace("\n", "").strip().replace(" ", "")
+            aux1 = content.split("make_node('")[1::2]
+            operator = [i.split("',")[0] for i in aux1]
+
+            if not operator:
+                print("Skipping file", case_full_path)
+                continue
+            else:
+                domain_name = [i.domain for i in ALL_SCHEMAS if i.name == operator[0]]
+
+            if not domain_name:
+                domain_name = "custom"
+                print("Operator", operator, "was not found, so it should be a custom")
+            else:
+                domain_name = domain_name[0]
+
+            # Base domain ai.onnx is empty
+            if domain_name == "":
+                domain_name = "ai.onnx"
+
+            # Just skip if a specific domain is provided
+            if domain and domain != domain_name:
+                continue
+
+            mappings = {"ai.onnx": version[2],
+                        "ai.onnx.ml": version[3],
+                        "ai.onnx.preview.training": version[4] if len(version) > 4 else '-1',
+                        # this field is optional
+                        "custom": 1}
+
+            os.makedirs(os.path.join(out_path, domain_name), exist_ok=True)
+            os.makedirs(os.path.join(out_path, domain_name, str(mappings[domain_name])), exist_ok=True)
+            shutil.copy(case_full_path, os.path.join(out_path, domain_name, str(mappings[domain_name])))
 
 
 def main():  # type: () -> None
     args = parse_args()
+
+    if not args.getdata and not args.getcase:
+        raise Exception("You must set either getdata or getcase")
 
     # If no release is provided, crawl the whole table
     if not args.release:
@@ -98,7 +184,8 @@ def main():  # type: () -> None
 
     # Iterate the PyPI releases
     for version in releases:
-        release = version[0] 
+        release = version[0]
+        
         # Download the Python release
         #download(release)
 
@@ -109,82 +196,20 @@ def main():  # type: () -> None
         onnx_tar_release.extractall(f"{TEMP_DIR}")
 
         # Path to the tests folder containing all the onnx and pb files
-        tests_path = os.path.join(folder_name, PYTHON_TESTS)     
+        tests_path = os.path.join(folder_name, PYTHON_TESTS)
         node_path = os.path.join(folder_name, PYTHON_CASE)
 
-        # Iterate test cases of a given release
-        for test_folder in os.listdir(tests_path):
-            test_folder_full = os.path.join(tests_path, test_folder)
-            if os.path.isdir(test_folder_full):
-
-                # Check if the test contains a model file. Note that onnx is used
-                # in the last releases but .pb is used in the first ones
-                included_extensions = ['model.onnx', 'node.pb']
-                file_names = [fn for fn in os.listdir(test_folder_full) if any(fn.endswith(ext) for ext in included_extensions)]
-
-                if len(file_names) != 1:
-                    raise Exception("Model file not found")
-
-                # Open the onnx file
-                onnx_path = os.path.join(tests_path, test_folder, file_names[0])
-                onnx_model = onnx.load(onnx_path)
-
-                if len(onnx_model.opset_import) != 1:
-                    raise Exception("Opset imported different than one")
-
-                # Check which opset is imported
-                opset_version = onnx_model.opset_import[0].version
-                domain_name = onnx_model.opset_import[0].domain
-
-                # Base domain ai.onnx is empty
-                if domain_name == "":
-                    domain_name = "ai.onnx"
-
-                # Just skip if a specific domain is provided
-                if args.domain and args.domain != domain_name:
-                    continue
-
-                copy_files(domain_name, opset_version, test_folder,
-                           test_folder_full, args.modelpath)
+        if args.getdata:
+            get_data(tests_path, args.domain, args.modelpath)
+            print("Done processing data for release", release)
 
         # if getcase is enabled, copy also the python scripts that generate the models
         if args.getcase:
-            for test in os.listdir(node_path):
-                if test != "__init__.py":                    
-                    case_full_path = os.path.join(folder_name, PYTHON_CASE, test)
+            get_node(node_path, args.domain, args.casepath, version)
+            print("Done processing scripts for release", release)
+    # Commented for testing
+    # os.system(f"rm -r {TEMP_DIR}")
 
-                    case_f = open(case_full_path, 'r')
-                    content = case_f.read().replace("\n", "").strip().replace(" ", "")
-                    aux1 = content.split("make_node('")[1::2]
-                    operator = [i.split("',")[0] for i in aux1]
-
-                    if not operator:
-                        print("Skipping file", case_full_path)
-                        continue
-                    else:
-                        domain = [i.domain for i in ALL_SCHEMAS if i.name == operator[0]]
-
-                    if not domain:
-                        domain = "custom"
-                        print("Operator", operator, "was not found, so it should be a custom")
-                    else:
-                        domain = domain[0]
-
-                    # Base domain ai.onnx is empty
-                    if domain == "":
-                        domain = "ai.onnx"
-                
-                    mappings = {"ai.onnx": version[2],
-                                "ai.onnx.ml": version[3],
-                                "ai.onnx.preview.training": version[4] if len(version) > 4 else '-1', # this field is optional
-                                "custom": 1}
-
-                    os.makedirs(os.path.join(args.casepath, domain), exist_ok=True)
-                    os.makedirs(os.path.join(args.casepath, domain, str(mappings[domain])), exist_ok=True)
-                    shutil.copy(case_full_path, os.path.join(args.casepath, domain, str(mappings[domain])))
-
-    #Commented for testing
-    #os.system(f"rm -r {TEMP_DIR}")
 
 if __name__ == '__main__':
     main()
