eb65f68234b3b622be6795170bd27ad9b3450605
alrevuelta <alvrevuelta@gmail.com>
2020-09-22 12:05:40 +0200

    minor refactoring
    minor refactoring

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,52 +2,51 @@ from ctypes import *
 import unittest
 
 connxr = CDLL('build/libconnxr.so')
+test_model_function = connxr.test_model
+test_model_function.restype = c_double
 
-
-def test_model(model_id, model_path, io_path, n_inputs, n_outputs):
-    test_model_function = connxr.test_model
-    test_model_function.restype = c_double
-    result = test_model_function(model_id, model_path, io_path, n_inputs, n_outputs)
-    if result > 0:
+class TestModel():
+    def test_model(self):
+        for testvector in self.io:
+            result = test_model_function(self.id.encode('UTF-8'),
+                                         self.path.encode('UTF-8'),
+                                         testvector.encode('UTF-8'),
+                                         self.n_inputs, self.n_outputs)
+        self.assertTrue(result)
         print("Inference time", result)
-    else:
-        print("Error, inference time is <0", result)
-    return result
+
+class TestMnist(TestModel, unittest.TestCase):
+    id = "mnist"
+    path = "test/mnist/model.onnx"
+    io = ["test/mnist/test_data_set_0",
+          "test/mnist/test_data_set_1",
+          "test/mnist/test_data_set_2"]
+    n_inputs = 1
+    n_outputs = 1
 
 
-class TestModels(unittest.TestCase):
-    def test_mnist(self):
-        self.assertTrue(test_model(b"mnist",
-                                   b"test/mnist/model.onnx",
-                                   b"test/mnist/test_data_set_0",
-                                   1, 1) > 0)
+class TestMobilenetv2(TestModel, unittest.TestCase):
+    id = "mobilenetv2"
+    path = "test/mobilenetv2-1.0/mobilenetv2-1.0.onnx"
+    io = ["test/mobilenetv2-1.0/test_data_set_0"]
+    n_inputs = 1
+    n_outputs = 1
 
-        self.assertTrue(test_model(b"mnist",
-                                   b"test/mnist/model.onnx",
-                                   b"test/mnist/test_data_set_1",
-                                   1, 1) > 0);
-        self.assertTrue(test_model(b"mnist",
-                                   b"test/mnist/model.onnx",
-                                   b"test/mnist/test_data_set_2",
-                                   1, 1) > 0);
 
-    def test_mobilenetv2(self):
-        self.assertTrue(test_model(b"mobilenetv2",
-                                   b"test/mobilenetv2-1.0/mobilenetv2-1.0.onnx",
-                                   b"test/mobilenetv2-1.0/test_data_set_0",
-                                   1, 1) > 0);
+class TestSuperresolution(TestModel, unittest.TestCase):
+    id = "super_resolution"
+    path = "test/super_resolution/super_resolution.onnx"
+    io = ["test/super_resolution/test_data_set_0"]
+    n_inputs = 1
+    n_outputs = 1
 
-    def test_super_resolution(self):
-        self.assertTrue(test_model(b"super_resolution",
-                                   b"test/super_resolution/super_resolution.onnx",
-                                   b"test/super_resolution/test_data_set_0",
-                                   1, 1) > 0);
 
-    def test_tinyyolov2(self):
-        self.assertTrue(test_model(b"tinyyolov2",
-                                   b"test/tiny_yolov2/Model.onnx",
-                                   b"test/tiny_yolov2/test_data_set_0",
-                                   1, 1) > 0);
+class TestTinyyolov2(TestModel, unittest.TestCase):
+    id = "tinyyolov2"
+    path = "test/tiny_yolov2/Model.onnx"
+    io = ["test/tiny_yolov2/test_data_set_0"]
+    n_inputs = 1
+    n_outputs = 1
 
 
 if __name__ == "__main__":
