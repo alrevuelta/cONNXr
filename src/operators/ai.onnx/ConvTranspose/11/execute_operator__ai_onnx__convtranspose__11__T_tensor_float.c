64e2ff714773b60ed3d1fda9645efd87c0103fee
Coderitter GmbH <schreib@coderitter.io>
2021-03-11 08:32:35 +0100

    Use executer_context to clean up code

--- src/operators/ai.onnx/ConvTranspose/11/execute_operator__ai_onnx__convtranspose__11__T_tensor_float.c
+++ src/operators/ai.onnx/ConvTranspose/11/execute_operator__ai_onnx__convtranspose__11__T_tensor_float.c
@@ -6,11 +6,6 @@
 //defined in prepare
 int calcOutputSize(int inputSize, int kernelSize, int stride, int dilations, int padStart, int padEnd);
 
-//transformes a 2d pos into a 1d flat array pos
-static inline int calcArrayPos2D(int x, int y, int width) {
-    return y * width + x;
-}
-
 //transformes a 3d pos into a 1d flat array pos
 static inline int calcArrayPos3D(int x, int y, int outputChannel, int width, int height) {
     return outputChannel * height * width + y * width + x;
@@ -43,15 +38,11 @@ execute_operator__ai_onnx__convtranspose__11__T_tensor_float(
     TRACE_TENSOR(2, true, i_W);
     TRACE_TENSOR(2, B, i_B);
 
-    Onnx__AttributeProto *a_dilations = searchAttributeNyName(ctx->onnx_node->n_attribute,ctx->onnx_node->attribute,"dilations");
-    Onnx__AttributeProto *a_pads = searchAttributeNyName(ctx->onnx_node->n_attribute,ctx->onnx_node->attribute,"pads");
-    Onnx__AttributeProto *a_strides = searchAttributeNyName(ctx->onnx_node->n_attribute,ctx->onnx_node->attribute,"strides");
-
-    // context_operator__ai_onnx__convtranspose__11 *op_ctx = ctx->executer_context;
+    context_operator__ai_onnx__convtranspose__11 *op_ctx = ctx->executer_context;
 
     // char* auto_pad = op_ctx->auto_pad;
-    // size_t n_dilations = op_ctx->n_dilations;
-    // int64_t* dilations = op_ctx->dilations;
+    size_t n_dilations = op_ctx->n_dilations;
+    int64_t* dilations = op_ctx->dilations;
     // int64_t group = op_ctx->group;
     // size_t n_kernel_shape = op_ctx->n_kernel_shape;
     // int64_t* kernel_shape = op_ctx->kernel_shape;
@@ -59,19 +50,19 @@ execute_operator__ai_onnx__convtranspose__11__T_tensor_float(
     // int64_t* output_padding = op_ctx->output_padding;
     // size_t n_output_shape = op_ctx->n_output_shape;
     // int64_t* output_shape = op_ctx->output_shape;
-    // size_t n_pads = op_ctx->n_pads;
-    // int64_t* pads = op_ctx->pads;
-    // size_t n_strides = op_ctx->n_strides;
-    // int64_t* strides = op_ctx->strides;
+    size_t n_pads = op_ctx->n_pads;
+    int64_t* pads = op_ctx->pads;
+    size_t n_strides = op_ctx->n_strides;
+    int64_t* strides = op_ctx->strides;
 
     // TRACE_VAR(2, true, auto_pad, "\"%s\"");
-    // TRACE_ARRAY(2, true, dilations, , n_dilations, "%" PRId64);
+    TRACE_ARRAY(2, true, dilations, , n_dilations, "%" PRId64);
     // TRACE_VAR(2, true, group, "%" PRId64);
     // TRACE_ARRAY(2, true, kernel_shape, , n_kernel_shape, "%" PRId64);
     // TRACE_ARRAY(2, true, output_padding, , n_output_padding, "%" PRId64);
     // TRACE_ARRAY(2, true, output_shape, , n_output_shape, "%" PRId64);
-    // TRACE_ARRAY(2, true, pads, , n_pads, "%" PRId64);
-    // TRACE_ARRAY(2, true, strides, , n_strides, "%" PRId64);
+    TRACE_ARRAY(2, true, pads, , n_pads, "%" PRId64);
+    TRACE_ARRAY(2, true, strides, , n_strides, "%" PRId64);
 
     Onnx__TensorProto *o_Y = searchOutputByName(ctx, 0);
     printf("%p\n", o_Y->float_data);
@@ -91,35 +82,16 @@ execute_operator__ai_onnx__convtranspose__11__T_tensor_float(
     const int kernelSizeY = i_W->dims[2];
     const int outputChannels = i_W->dims[1];
 
-    int strideX = 1;
-    int strideY = 1;
+    const int strideX = strides[1];
+    const int strideY = strides[0];
 
-    if(a_strides != NULL) {
-        strideX = a_strides->ints[1];
-        strideY = a_strides->ints[0];
-    }
+    const int dilationsX = dilations[1];
+    const int dilationsY = dilations[0];
 
-    int dilationsX = 1;
-    int dilationsY = 1;
-
-    if(a_dilations != NULL) {
-        dilationsX = a_dilations->ints[1];
-        dilationsY = a_dilations->ints[0];
-    }
-
-    int padStartY = 0;
-    int padStartX = 0;
-    int padEndY = 0;
-    int padEndX = 0;
-
-    if(a_pads != NULL) {
-        assert(a_pads->n_ints == 4);
-
-        padStartY = a_pads->ints[0];
-        padStartX = a_pads->ints[1];
-        padEndY = a_pads->ints[2];
-        padEndX = a_pads->ints[3];
-    }
+    const int padStartY = pads[0];
+    const int padStartX = pads[1];
+    const int padEndY = pads[2];
+    const int padEndX = pads[3];
 
     const int outputSizeX = calcOutputSize(inputSizeX, kernelSizeX, strideX, dilationsX, padStartX, padEndX);
     const int outputSizeY = calcOutputSize(inputSizeY, kernelSizeY, strideY, dilationsY, padStartY, padEndY);
@@ -128,11 +100,7 @@ execute_operator__ai_onnx__convtranspose__11__T_tensor_float(
 
     //fill with bias
     for(int c=0; c<outputChannels; c++) {
-        float bias = 0;
-        if(i_B != NULL) {
-            bias = i_B->float_data[c];
-        }
-        //float bias = i_B?i_B->float_data[c]:0;
+        float bias = i_B?i_B->float_data[c]:0;
         for(int y=0; y<outputSizeY; y++) {
             for(int x=0; x<outputSizeX; x++) {
                 output[calcArrayPos3D(x,y,c,outputSizeX, outputSizeY)] = bias;
@@ -176,6 +144,5 @@ execute_operator__ai_onnx__convtranspose__11__T_tensor_float(
     TRACE_EXIT(1);
 
     /* CHANGE RETURN CODE IF THIS EXECUTER IS VALID */
-    //return OP_ENOSYS;
     return OP_OK;
 }
--- src/operators/ai.onnx/ConvTranspose/11/prepare_operator__ai_onnx__convtranspose__11.c
+++ src/operators/ai.onnx/ConvTranspose/11/prepare_operator__ai_onnx__convtranspose__11.c
@@ -7,6 +7,14 @@ int calcOutputSize(int inputSize, int kernelSize, int stride, int dilations, int
     return stride * (inputSize - 1) + ((kernelSize - 1) * dilations + 1 - padStart - padEnd);
 }
 
+int64_t* createArray(size_t n, int64_t value) {
+    int64_t* p = (int64_t*)malloc(n * sizeof(int64_t));
+    for(int i = 0; i < n; i++) {
+        p[i] = value;
+    }
+    return p;
+}
+
 operator_status
 prepare_operator__ai_onnx__convtranspose__11(
     node_context *ctx
@@ -22,9 +30,9 @@ prepare_operator__ai_onnx__convtranspose__11(
     Onnx__TensorProto *i_W = searchInputByName(ctx, 1);
     Onnx__TensorProto *i_B = searchInputByName(ctx, 2);
 
-    // TRACE_TENSOR(2, true, i_X);
-    // TRACE_TENSOR(2, true, i_W);
-    // TRACE_TENSOR(2, B, i_B);
+    TRACE_TENSOR(2, true, i_X);
+    TRACE_TENSOR(2, true, i_W);
+    TRACE_TENSOR(2, B, i_B);
 
     // Onnx__AttributeProto *a_auto_pad = searchAttributeNyName(ctx->onnx_node->n_attribute,ctx->onnx_node->attribute,"auto_pad");
     Onnx__AttributeProto *a_dilations = searchAttributeNyName(ctx->onnx_node->n_attribute,ctx->onnx_node->attribute,"dilations");
@@ -38,111 +46,55 @@ prepare_operator__ai_onnx__convtranspose__11(
     Onnx__AttributeProto *a_strides = searchAttributeNyName(ctx->onnx_node->n_attribute,ctx->onnx_node->attribute,"strides");
 
     // TRACE_ATTRIBUTE(2, a_auto_pad, a_auto_pad);
-    // TRACE_ATTRIBUTE(2, a_dilations, a_dilations);
-    // TRACE_ATTRIBUTE(2, a_group, a_group);
+    TRACE_ATTRIBUTE(2, a_dilations, a_dilations);
+    TRACE_ATTRIBUTE(2, a_group, a_group);
     // TRACE_ATTRIBUTE(2, a_kernel_shape, a_kernel_shape);
     // TRACE_ATTRIBUTE(2, a_output_padding, a_output_padding);
     // TRACE_ATTRIBUTE(2, a_output_shape, a_output_shape);
-    // TRACE_ATTRIBUTE(2, a_pads, a_pads);
-    // TRACE_ATTRIBUTE(2, a_strides, a_strides);
+    TRACE_ATTRIBUTE(2, a_pads, a_pads);
+    TRACE_ATTRIBUTE(2, a_strides, a_strides);
 
     Onnx__TensorProto *o_Y = searchOutputByName(ctx, 0);
 
     /* ALLOCATE AND INITIALIZE CONTEXT HERE IF NEEDED */
 
-    // char* default_auto_pad = ;
-    // size_t default_n_dilations = ;
-    // int64_t* default_dilations = ;
-
-    int64_t default_group;
-    if(a_group == NULL) {
-        default_group = 1;
-    } else {
-
-        //we cant handle groups != 1
-        if(a_group->i != 1) {
-            return OP_EDOM;
-        }
-
-        default_group = a_group->i;
-    }
-
-    if(i_X->n_dims != 4) {
-        printf("%zu\n", i_X->n_dims);
-        printf("please use convtranspose only with a 2D input (4D tensor)\n");
-        return OP_EDOM;
-    }
-
-    if(i_X->dims[0]!= 1) {
-        printf("please use convtranspose only with batchsize of 1\n");
-        return OP_EDOM;
-    }
+    TRACE_FATAL(0, i_X->n_dims != 4, "use convtranspose only with a 2D input (4D tensor), %zu was given", i_X->n_dims);
+    TRACE_FATAL(0, i_X->dims[0]!= 1, "use convtranspose only with batchsize of 1, %zu was given", i_X->dims[0]);
 
     int inputSizeX = i_X->dims[3];
     int inputSizeY = i_X->dims[2];
     int inputChannels = i_X->dims[1];
 
-    if(inputChannels != i_W->dims[0]) {
-        printf("size of input channels needs to be qual with weights\n");
-        return OP_EINVAL;
-    }
+    TRACE_FATAL(0, inputChannels != i_W->dims[0], "size of input channels needs to be qual with weights");
     
     int kernelSizeX = i_W->dims[3];
     int kernelSizeY = i_W->dims[2];
     int outputChannels = i_W->dims[1];
 
-        if(i_B != NULL) {
+    if(i_B != NULL) {
         int biasSize = i_B->dims[0];
-        if(biasSize != i_W->dims[1] * default_group) {
-            printf("size of input channels needs to be qual with weights\n");
-            return OP_EINVAL;
-        }
+        TRACE_FATAL(0, biasSize != i_W->dims[1] * default_group, "size of input channels needs to be qual with bias");
     }
 
-    int strideX = 1;
-    int strideY = 1;
-
-    if(a_strides != NULL) {
-        strideX = a_strides->ints[1];
-        strideY = a_strides->ints[0];
-    }
-
-    int dilationsX = 1;
-    int dilationsY = 1;
-
-    if(a_dilations != NULL) {
-        dilationsX = a_dilations->ints[1];
-        dilationsY = a_dilations->ints[0];
-    }
-
-    int padStartY = 0;
-    int padStartX = 0;
-    int padEndY = 0;
-    int padEndX = 0;
-
-    if(a_pads != NULL) {
-        if(a_pads->n_ints != 4) {
-            return OP_EDOM;
-        }
-
-        padStartY = a_pads->ints[0];
-        padStartX = a_pads->ints[1];
-        padEndY = a_pads->ints[2];
-        padEndX = a_pads->ints[3];
-    }
-
-    int outputSizePaddedX = calcOutputSize(inputSizeX, kernelSizeX, strideX, dilationsX, padStartX, padEndX);
-    int outputSizePaddedY = calcOutputSize(inputSizeY, kernelSizeY, strideY, dilationsY, padStartY, padEndY);
+    //number of tensor axes minus batch minus channel 
+    int dim = i_X->n_dims - 2;
     
+    int64_t default_group = 1;
+    // char* default_auto_pad = ;
+    size_t default_n_dilations = dim;
+    int64_t default_dilation = 1;
+    // int64_t* default_dilations = ;
     // size_t default_n_kernel_shape = ;
     // int64_t* default_kernel_shape = ;
     // size_t default_n_output_padding = ;
     // int64_t* default_output_padding = ;
     // size_t default_n_output_shape = ;
     // int64_t* default_output_shape = ;
-    // size_t default_n_pads = ;
+    size_t default_n_pads = 2 * dim;
+    int64_t default_pad = 0;
     // int64_t* default_pads = ;
-    // size_t default_n_strides = ;
+    size_t default_n_strides = dim;
+    int64_t default_stride = 1;
     // int64_t* default_strides = ;
 
     context_operator__ai_onnx__convtranspose__11 *op_ctx = NULL;
@@ -150,10 +102,13 @@ prepare_operator__ai_onnx__convtranspose__11(
     TRACE_FATAL(0 , !op_ctx, "could not allocate executer_context");
 
     // op_ctx->auto_pad = a_auto_pad?strndup((char*)a_auto_pad->s.data, a_auto_pad->s.len):default_auto_pad;
-    // op_ctx->n_dilations = a_dilations?a_dilations->n_ints:default_n_dilations;
-    // op_ctx->dilations = a_dilations?a_dilations->ints:ARRAYDUP(default_dilations,default_n_dilations);
-    // TRACE_FATAL(0, !op_ctx->dilations, "malloc failed");
-    // op_ctx->group = a_group?a_group->i:default_group;
+    op_ctx->n_dilations = a_dilations?a_dilations->n_ints:default_n_dilations;
+    op_ctx->dilations = a_dilations?ARRAYDUP(a_dilations->ints,default_n_dilations):createArray(default_n_dilations, default_dilation);
+    TRACE_FATAL(0, !op_ctx->dilations, "malloc failed");
+    op_ctx->group = a_group?a_group->i:default_group;
+
+    TRACE_FATAL(0, op_ctx->group != 1, "use convtranspose only with a groupsize of 1, %zu was given", op_ctx->group);
+
     // op_ctx->n_kernel_shape = a_kernel_shape?a_kernel_shape->n_ints:default_n_kernel_shape;
     // op_ctx->kernel_shape = a_kernel_shape?a_kernel_shape->ints:ARRAYDUP(default_kernel_shape,default_n_kernel_shape);
     // TRACE_FATAL(0, !op_ctx->kernel_shape, "malloc failed");
@@ -163,40 +118,53 @@ prepare_operator__ai_onnx__convtranspose__11(
     // op_ctx->n_output_shape = a_output_shape?a_output_shape->n_ints:default_n_output_shape;
     // op_ctx->output_shape = a_output_shape?a_output_shape->ints:ARRAYDUP(default_output_shape,default_n_output_shape);
     // TRACE_FATAL(0, !op_ctx->output_shape, "malloc failed");
-    // op_ctx->n_pads = a_pads?a_pads->n_ints:default_n_pads;
-    // op_ctx->pads = a_pads?a_pads->ints:ARRAYDUP(default_pads,default_n_pads);
-    // TRACE_FATAL(0, !op_ctx->pads, "malloc failed");
-    // op_ctx->n_strides = a_strides?a_strides->n_ints:default_n_strides;
-    // op_ctx->strides = a_strides?a_strides->ints:ARRAYDUP(default_strides,default_n_strides);
-    // TRACE_FATAL(0, !op_ctx->strides, "malloc failed");
+    op_ctx->n_pads = a_pads?a_pads->n_ints:default_n_pads;
+    op_ctx->pads = a_pads?ARRAYDUP(a_pads->ints,op_ctx->n_pads):createArray(default_n_pads, default_pad);
+    TRACE_FATAL(0, !op_ctx->pads, "malloc failed");
+    op_ctx->n_strides = a_strides?a_strides->n_ints:default_n_strides;
+    op_ctx->strides = a_strides?ARRAYDUP(a_strides->ints,default_n_strides):createArray(default_n_strides, default_stride);
+    TRACE_FATAL(0, !op_ctx->strides, "malloc failed");
 
     // TRACE_VAR(2, true, op_ctx->auto_pad, "\"%s\"");
-    // TRACE_ARRAY(2, true, op_ctx->dilations, , op_ctx->n_dilations, "%" PRId64);
+    TRACE_ARRAY(2, true, op_ctx->dilations, , op_ctx->n_dilations, "%" PRId64);
     // TRACE_VAR(2, true, op_ctx->group, "%" PRId64);
     // TRACE_ARRAY(2, true, op_ctx->kernel_shape, , op_ctx->n_kernel_shape, "%" PRId64);
     // TRACE_ARRAY(2, true, op_ctx->output_padding, , op_ctx->n_output_padding, "%" PRId64);
     // TRACE_ARRAY(2, true, op_ctx->output_shape, , op_ctx->n_output_shape, "%" PRId64);
-    // TRACE_ARRAY(2, true, op_ctx->pads, , op_ctx->n_pads, "%" PRId64);
-    // TRACE_ARRAY(2, true, op_ctx->strides, , op_ctx->n_strides, "%" PRId64);
+    TRACE_ARRAY(2, true, op_ctx->pads, , op_ctx->n_pads, "%" PRId64);
+    TRACE_ARRAY(2, true, op_ctx->strides, , op_ctx->n_strides, "%" PRId64);
 
     /* INITIALIZE OUTPUTS DATA_TYPE AND SHAPE HERE */
 
+    const int strideX = op_ctx->strides[1];
+    const int strideY = op_ctx->strides[0];
+
+    const int dilationsX = op_ctx->dilations[1];
+    const int dilationsY = op_ctx->dilations[0];
+
+    const int padStartY = op_ctx->pads[0];
+    const int padStartX = op_ctx->pads[1];
+    const int padEndY = op_ctx->pads[2];
+    const int padEndX = op_ctx->pads[3];
+
+    int outputSizeX = calcOutputSize(inputSizeX, kernelSizeX, strideX, dilationsX, padStartX, padEndX);
+    int outputSizeY = calcOutputSize(inputSizeY, kernelSizeY, strideY, dilationsY, padStartY, padEndY);
 
     o_Y->n_dims       = i_X->n_dims;
     o_Y->dims         = malloc(o_Y->n_dims * sizeof(int64_t));
     o_Y->has_raw_data = 0;
     o_Y->data_type    = ONNX__TENSOR_PROTO__DATA_TYPE__FLOAT;
-    o_Y->n_float_data = outputSizePaddedX*outputSizePaddedY*outputChannels;
+    o_Y->n_float_data = outputSizeX * outputSizeY * outputChannels;
 
     o_Y->dims[0] = 1;
     o_Y->dims[1] = outputChannels;
-    o_Y->dims[2] = outputSizePaddedY;
-    o_Y->dims[3] = outputSizePaddedX;
+    o_Y->dims[2] = outputSizeY;
+    o_Y->dims[3] = outputSizeX;
 
     /* MALLOC OUTPUT TENSORS HERE */
 
     //mallocTensorData(o_Y);
-    o_Y->float_data = (float*)malloc(outputSizePaddedX * outputSizePaddedY * outputChannels * sizeof(float));
+    o_Y->float_data = (float*)malloc(outputSizeX * outputSizeY * outputChannels * sizeof(float));
 
     // TRACE_TENSOR(2, true, o_Y);
 
